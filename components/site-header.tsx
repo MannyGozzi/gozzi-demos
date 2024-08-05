953a41291e13a552f715a2bb120ddd3165440759
Manuel Gozzi <mannygozzidev@protonmail.com>
2024-08-04 18:28:46 -0700

    Fixed build issues

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -10,7 +10,7 @@ export function SiteHeader() {
   return (
     <header className="bg-background/60 backdrop-blur-2xl fixed top-0 z-40 w-full">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <MainNav items={siteConfig.mainNav} />
+        <MainNav/>
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
             <Link
